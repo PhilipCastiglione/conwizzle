ce895d156bfd2f6ab40685d145e6f3353fe866e6
Philip Castiglione <philipcastiglione@gmail.com>
2015-07-16 20:25:50 +1000

    made some functions and stuff

--- conwizzle.js
+++ conwizzle.js
@@ -1,25 +1,24 @@
-// GLOBALS
-var grid = [];
-
 $(document).ready( function() {
 
+  var grid = [];
   // ESTABLISH GRID
-  for (var i = 0; i < 50; i++) {
+  var size = 50;
+  for (var r = 0; r < size; r++) {
     var row = [];
-    for (var i2 = 0; i2 < 50; i2++) {
+    for (var c = 0; c < size; c++) {
       row.push([0,0]);
     }
     grid.push(row);
   }
 
-  function render() {
+  function render(grid) {
     var $grid = $("<div class='grid'>"),
         $row,
         state;
-    for (var i = 0; i < 50; i++) {
+    for (var r = 0; r < size; r++) {
     $row = $("<div class='row'>");
-    for (var i2 = 0; i2 < 50; i2++) {
-      state = (grid[i][i2][0] == 1)? 'alive' : 'dead';
+    for (var c = 0; c < size; c++) {
+      state = (grid[r][c][0] == 1)? 'alive' : 'dead';
       $row.append("<div class='col " + state + "'>");
     }
     $grid.append($row);
@@ -27,12 +26,56 @@ $(document).ready( function() {
     $('.container').html($grid);
   }
 
-  function ITSALIIIVE(row, col) {
-    grid[row][col] = 1;
+  function progressCells(grid) {
+    var nextState, cell;
+    for (var r = 0; r < size; r++) {
+      for (var c = 0; c < size; c++) {
+        cell = grid[r][c];
+        nextState = determineState(grid, cell, r, c);
+        grid[r][c] = progressCell(cell, nextState);
+      }
+    }
   }
 
-  function KILLTHEFUCKER(row, col) {
-    grid[row][col] = 0;
+  function determineState(grid, cell, row, col) {
+    var score = getCellScore(grid, row, col);
+    if (cell[0] = 1) {
+      if (score < 2 || score > 3) {
+        return 0;
+      } else {
+        return 1;
+      }
+    } else {
+      if (score != 3) {
+        return 0;
+      } else {
+        return 1;
+      }
+    }
   }
+
+  function getCellScore(grid, row, col) {
+    var count;
+    for (var x = 0; x < 9; x++) {
+      count += grid[row][col][0];
+    }
+    return count;
+  }
+
+  function progressCell(ary, nextState) {
+    return [ary[1], nextState];
+  }
+
+  function tick() {
+    progressCells(grid);
+    render(grid);
+  }
+
+  render(grid);
+  var intervalId = setInterval(tick, 500);
 
 });
+
+// include timer in / stage of evolution
+// include controls to stop/pause/restart/modify params
+// make a pretty splash for the text
